d13de6e355fed78a72b9973377b2ed2c278057b1
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-08 17:34:41 +0300

    Style header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,24 +1,28 @@
 import Link from "next/link"
 
 const Header = () => {
-  return (
-    <header className="flex justify-between">
-        <div>
-            <Link href="/">Wuletaw Wonte</Link>
-        </div>
 
-        <nav>
-            <ul className="flex gap-2">
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
-  )
+    return (
+        <header className="flex justify-between pt-4 pb-10 leading-9 text-xl">
+            <div> 
+                <Link href="/" className="shadow-none no-underline font-bold text-center leading-[0.5rem] tracking-widest">Wuletaw Wonte</Link>
+            </div>
+
+            <nav>
+                <ul className="flex items-center gap-4">
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">Blog</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
