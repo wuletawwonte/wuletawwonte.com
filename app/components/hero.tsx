a8e8750d508a3147a86bd4997453423bdc91e220
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-08 19:05:02 +0300

    Grow the flex property of hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,10 +3,15 @@ import avatar from '../../public/me.png'
 
 const Hero = () => {
   return (
-    <div className='flex text-xl leading-11 mt-16 mb-20'>
-        <Image src={avatar} 
-        alt="Picture of Wuletaw Wonte" 
-        className="w-36 h-36 min-w-36"></Image>
+    <div className='flex gap-3 text-xl leading-11 mt-16 mb-20'>
+        <div className="rounded-full border-orange-900 border-2 p-1">
+            <Image src={avatar} 
+            alt="Picture of Wuletaw Wonte" 
+            className="w-36 h-36 min-w-36"></Image>
+        </div>
+        <div className="grow border border-red">
+            <h1 className="font-bold">Wuletaw Wonte</h1>            
+        </div>
     </div>
   )
 }
