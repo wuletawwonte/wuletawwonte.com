c9a5444aa3664f6b5ea37997b64cd1f20c3f450e
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-09 19:04:11 +0300

    Add greeting message

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -4,13 +4,13 @@ import avatar from '../../public/me.png'
 const Hero = () => {
   return (
     <div className='flex gap-3 text-xl leading-11 mt-16 mb-20'>
-        <div className="rounded-full border-orange-900 border-2 p-1">
+        <div className="w-44 rounded-full border-orange-900 border-2 p-1">
             <Image src={avatar} 
             alt="Picture of Wuletaw Wonte" 
             className="w-36 h-36 min-w-36"></Image>
         </div>
         <div className="grow border border-red">
-            <h1 className="font-bold">Wuletaw Wonte</h1>            
+            <h1 className="font-bold">Hello, I'm Wuletaw, a full stack web developer based in Arba Minch. </h1>            
         </div>
     </div>
   )
