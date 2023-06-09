e3671f4b4a72f43ea801d8a975d7d8bfa4e0fa14
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-09 20:55:12 +0300

    Stle hero section avatar element

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,13 +1,12 @@
+import "./hero.scss"
 import Image from 'next/image'
 import avatar from '../../public/me.png'
 
 const Hero = () => {
   return (
-    <div className='flex gap-3 text-xl leading-11 mt-16 mb-20'>
-        <div className="w-44 rounded-full border-orange-900 border-2 p-1">
-            <Image src={avatar} 
-            alt="Picture of Wuletaw Wonte" 
-            className="w-36 h-36 min-w-36"></Image>
+    <div className='hero'>
+        <div className="hero__avatar">
+            <Image src={avatar} alt="Picture of Wuletaw Wonte"></Image>
         </div>
         <div className="grow border border-red">
             <h1 className="font-bold">Hello, I'm Wuletaw, a full stack web developer based in Arba Minch. </h1>            
