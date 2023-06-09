6172c5b69ab4e0a1e647187d0cfa44a5a86ce337
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-09 20:32:43 +0300

    Style globals.scss file

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-custom-black text-custom-white m-auto max-w-3xl p-6`}>
+      <body>
         <Header />
         <main>
           {children}
