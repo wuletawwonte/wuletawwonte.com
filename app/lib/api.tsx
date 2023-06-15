4e2e7a5076afa454782182784f0f7df185c66faa
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-15 17:28:58 +0300

    Fetch post data from the posts page

--- app/lib/api.tsx
+++ app/lib/api.tsx
@@ -12,8 +12,7 @@ export function getPostSlugs() {
 }
 
 export function getPostBySlug(slug: string, fields: string[] = []) {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
+  const fullPath = join(postsDirectory, `${slug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
 
@@ -26,7 +25,7 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
     if (field === 'slug') {
-      items[field] = realSlug
+      items[field] = slug
     }
     if (field === 'content') {
       items[field] = content
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,9 +1,13 @@
+import { getPostBySlug } from '../../lib/api'
+
 const PostPage = (props: any) => {
-    const slug = props.params.slug
+  const slug = props.params.slug
+  const post = getPostBySlug(slug, ['title', 'content'])
   return (
     <>
     <h2>PostPage</h2>
     <p>{slug}</p> 
+    <p>{post.content}</p>
     </>   
   )
 }
