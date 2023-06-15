1c0ae38ee06c59ac681f6e403391905058afb50a
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-15 16:28:47 +0300

    Use the api code from the vercel organization

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { getPostSlugs } from "../lib/api"
 
 const BlogPage = () => {
-  return (
-    <div>Blog page</div>
+  const cwd = getPostSlugs()
+
+  return (    
+    <div>Blog page {cwd}</div>
   )
 }
 
