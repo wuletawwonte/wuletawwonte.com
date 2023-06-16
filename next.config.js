983eeb5a7162d894d22820b5b5655f28f202b3b1
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-06-16 23:07:22 +0300

    Static app deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    distDir: '_static',
+    images: {
+      unoptimized: true
+    }
+}
 
 module.exports = nextConfig
