7828d66975601434ca5b0ee29bb55f02adbd9d5c
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-02-20 09:24:03 +0300

    implement toggle color mode

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col">
     <Header />
-    <NuxtPage />
+    <div class="lg:px-56 px-2 pt-10">
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,21 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const toggleColorMode = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  colorMode.value = target.checked ? 'dark' : 'light';
+};
+</script>
+
 <template>
-  <div class="navbar bg-base-100 lg:px-24">
+  <div class="navbar bg-base-100 lg:px-24 pr-4">
     <div class="navbar-start">
       <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />
-          </svg>
+        <div tabindex="0" role="button" class="btn btn-link lg:hidden">
+          <Icon
+            name="material-symbols:menu"
+            class="w-6 h-6 text-base-content"
+          />
         </div>
         <ul
           tabindex="0"
@@ -33,7 +32,10 @@
           <li><a>Item 3</a></li>
         </ul>
       </div>
-      <a class="btn btn-ghost text-xl">Wuletaw Wonte</a>
+      <a
+        class="btn btn-link text-base-content no-underline hover:no-underline text-xl"
+        >Wuletaw Wonte</a
+      >
     </div>
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
@@ -52,13 +54,8 @@
     </div>
     <div class="navbar-end">
       <label class="swap swap-rotate">
-        <!-- this hidden checkbox controls the state -->
-        <input type="checkbox" class="theme-controller" value="dark" />
-
-        <!-- sun icon -->
+        <input type="checkbox" @change="toggleColorMode" />
         <Icon name="material-symbols-light:sunny" class="swap-on w-6 h-6" />
-
-        <!-- moon icon -->
         <Icon
           name="material-symbols-light:mode-night"
           class="swap-off w-6 h-6"
