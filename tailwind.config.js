8b876049e8bf55831e49d73f8b1f80a2a0b4caef
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-02-23 17:33:21 +0300

    Create hero section of the website (#15)
    * add tailwind typography plugin
    
    * install tailwind class ordering prettier plugin
    
    * add hover toggle on my name
    
    * add pronounciation of Wuletaw Wonte
    
    * add github link in the hero section
    
    * increase layout horizontal margin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bitter: ["Bitter", "serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+        sacramento: ["Sacramento", "sans-serif"],
+      },
+    },
   },
-  plugins: [require('daisyui')],
-  darkMode: 'class',
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  darkMode: "class",
 };
