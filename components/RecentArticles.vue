118c95da70da597c3f7d6789aaa4c46f6c632287
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-01 11:40:59 +0300

    add outline on active state

--- components/RecentArticles.vue
+++ components/RecentArticles.vue
@@ -1,6 +1,7 @@
 <script setup type="ts">
 
-const { data: articles } = await useAsyncData("articles", () => queryContent("blog").where({draft: {$eq: false}}).sort({ datePublished: -1}).find());
+const articlesCount = await queryContent("blog").where({draft: {$eq: false}}).count();
+const articles = await queryContent("blog").where({draft: {$eq: false}}).limit(5).sort({ datePublished: -1}).find();
 </script>
 
 <template>
@@ -16,5 +17,13 @@ const { data: articles } = await useAsyncData("articles", () => queryContent("bl
         :date="article.datePublished"
       />
     </ul>
+    <nuxt-link
+      v-if="articlesCount > 5"
+      to="/blog"
+      class="mt-8 flex items-center self-start text-xl text-primary text-opacity-80 hover:text-opacity-100 active:outline-dotted active:outline-2 active:outline-offset-2"
+    >
+      <span>All Articles</span>
+      <Icon name="fa6-solid:angle-right" class="ml-1 h-4 w-4" />
+    </nuxt-link>
   </div>
 </template>
