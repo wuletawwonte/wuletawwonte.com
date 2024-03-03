58590e540bb1211faf8e2f23a38a4dcad61ad944
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-03 12:05:25 +0300

    add projects content and design

---
+++ components/Project.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  description: String,
+  href: String,
+  image: String,
+});
+</script>
+
+<template>
+  <li class="group relative flex flex-col items-start rounded-lg">
+    <div
+      class="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0"
+    >
+      <img
+        alt=""
+        loading="lazy"
+        width="32"
+        height="32"
+        decoding="async"
+        data-nimg="1"
+        class="h-8 w-8"
+        style="color: transparent"
+        src="/images/me.jpeg"
+      />
+    </div>
+    <h2 class="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+      <div
+        class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+      ></div>
+      <a :href="href" target="_blank">
+        <span
+          class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"
+        ></span
+        ><span class="relative z-10">{{ title }}</span>
+      </a>
+    </h2>
+    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+      {{ description }}
+    </p>
+    <p
+      class="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200"
+    >
+      <icon name="akar-icons:link-chain" class="h-5 w-5 flex-none" />
+      <span class="ml-2">{{ href }}</span>
+    </p>
+  </li>
+</template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,3 +1,54 @@
+<script setup lang="ts">
+const projects = [
+  {
+    title: "wuletawwonte.com",
+    description: "A personal blog built with Nuxt.js and DaisyUI.",
+    href: "https://wuletawwonte.com",
+    image: "/images/wuletaw-wonte.png",
+  },
+  {
+    title: "Wuletaw Wonte",
+    description: "A personal blog built with Nuxt.js and Tailwind CSS.",
+    href: "https://wuletawwonte.com",
+    image: "/images/wuletaw-wonte.png",
+  },
+  {
+    title: "Wuletaw Wonte",
+    description: "A personal blog built with Nuxt.js and Tailwind CSS.",
+    href: "https://wuletawwonte.com",
+    image: "/images/wuletaw-wonte.png",
+  },
+];
+</script>
+
 <template>
-  <h2>This is projects page</h2>
+  <div>
+    <div class="max-w-2xl">
+      <h1
+        class="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
+      >
+        My side projects.
+      </h1>
+      <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+        I’ve worked on many little projects over the years but these are the
+        ones that I’m most proud of. Many of them are open-source, check out the
+        code and contribute if you are interested.
+      </p>
+    </div>
+    <div class="mt-16 sm:mt-20">
+      <ul
+        role="list"
+        class="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+      >
+        <Project
+          v-for="project in projects"
+          :key="project.title"
+          :title="project.title"
+          :description="project.description"
+          :href="project.href"
+          :image="project.image"
+        />
+      </ul>
+    </div>
+  </div>
 </template>
