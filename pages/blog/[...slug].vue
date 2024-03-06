a42b6efb08ac805ced87b6115118f08d873ceb24
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-03-06 18:19:13 +0300

    Create resume page (#39)
    * create empty resume page
    
    * fix page width issue
    
    * add experience section in my resume markdown
    
    * add remaining resume data

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
   <main>
-    <ContentDoc class="prose px-4 lg:prose-xl lg:px-8" />
+    <ContentDoc class="prose px-4 lg:prose-xl" />
   </main>
 </template>
---
+++ pages/resume/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const resume = ref(null);
+</script>
+
+<template>
+  <div
+    class="prose flex max-w-none flex-col font-ubuntu prose-hr:border-2 prose-hr:border-dashed"
+  >
+    <ContentDoc path="resume">
+      <template #empty>
+        <div class="flex h-96 items-center justify-center">
+          <div class="flex flex-col items-center">
+            <p>No resume document found</p>
+          </div>
+        </div>
+      </template>
+    </ContentDoc>
+  </div>
+</template>
